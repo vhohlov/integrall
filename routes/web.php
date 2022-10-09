d84e29abc7b8fa501fbecaca77afc0bca562628e
vihohlov <victor.hohlov@omniplay.io>
2022-10-09 13:10:03 +0300

    test redis

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,5 @@ Route::get('/', function () {
 Route::get('/test', function () {
     return "Hello user!";
 });
+
+Route::get('/user', [UserController::class, 'index']);
